6a447f330dad5e3074a9b3b446a6c1a3163dbdf9
David Redondo <rdnd201@gmail.com>
2020-10-01 11:51:32 +0200

    Update: add image placeholder while loading main view

---
+++ src/components/ImageWithLoading.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const ImageWithLoading = ({ src, alt }) => {
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+  return (
+    <>
+      {!isImageLoaded ? (
+        <svg width="362.66" height="271.98" xmlns="http://www.w3.org/2000/svg">
+          <g>
+            <title>background</title>
+            <rect
+              fill="#cccccc"
+              id="canvas_background"
+              height="273.98"
+              width="364.66"
+              y="-1"
+              x="-1"
+            />
+            <g
+              display="none"
+              overflow="visible"
+              y="0"
+              x="0"
+              height="100%"
+              width="100%"
+              id="canvasGrid"
+            >
+              <rect
+                fill="url(#gridpattern)"
+                stroke-width="0"
+                y="0"
+                x="0"
+                height="100%"
+                width="100%"
+              />
+            </g>
+          </g>
+          <g>
+            <title>Layer 1</title>
+          </g>
+        </svg>
+      ) : // <svg width="100" height="100" viewBox="0 0 100 100">
+      //   <rect width="100" height="100" rx="10" ry="10" fill="#CCC" />
+      // </svg>
+      null}
+      <img
+        style={!isImageLoaded ? { visibility: "hidden" } : {}}
+        src={src}
+        alt={alt}
+        onLoad={() => setIsImageLoaded(true)}
+      />
+    </>
+  );
+};
+
+ImageWithLoading.propTypes = {
+  src: PropTypes.string,
+  alt: PropTypes.string,
+};
+
+ImageWithLoading.defaultProps = {
+  src: "",
+  alt: "",
+};
+
+export default ImageWithLoading;
--- src/components/OompaCard.jsx
+++ src/components/OompaCard.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import genderConversionObject from "../utils/genderConversionObject";
+import ImageWithLoading from "./ImageWithLoading";
 
 const OompaCard = ({ oompa }) => {
   return (
     <Link to={`/${oompa.id}`}>
       <article>
-        <img src={oompa.image} alt="oompa's file" />
+        <ImageWithLoading src={oompa.image} alt="oompa's file" />
         <h2 className="text-2xl">{oompa.first_name}</h2>
         <div className="flex flex-col text-gray-500 italic text-sm">
           <span>{genderConversionObject[oompa.gender || "F"]}</span>
@@ -27,7 +28,7 @@ OompaCard.defaultProps = {
     first_name: "Unkown",
     gender: "F",
     id: 0,
-    image: "https://s3.eu-central-1.amazonaws.com/napptilus/level-test/1.jpg",
+    image: "",
     last_name: "Unkown",
     profession: "Unkown",
   },
