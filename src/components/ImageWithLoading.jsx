c446d0fe23512304ec137a00783c976ba7aa4d41
David Redondo <rdnd201@gmail.com>
2020-10-02 13:20:42 +0200

    Update: ImageWithLoading add spinner + match design dimensions

--- src/components/ImageWithLoading.jsx
+++ src/components/ImageWithLoading.jsx
@@ -1,56 +1,29 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import LoaderComponent from "./LoaderComponent";
+
+const hiddenStyle = { visibility: "hidden" };
+const containerStyle = { width: "355.5px", height: "218px" };
 
 const ImageWithLoading = ({ src, alt }) => {
   const [isImageLoaded, setIsImageLoaded] = useState(false);
   return (
-    <>
+    <div
+      className="overflow-hidden mb-6 flex items-center justify-center"
+      style={containerStyle}
+    >
       {!isImageLoaded ? (
-        <svg width="362.66" height="271.98" xmlns="http://www.w3.org/2000/svg">
-          <g>
-            <title>background</title>
-            <rect
-              fill="#cccccc"
-              id="canvas_background"
-              height="273.98"
-              width="364.66"
-              y="-1"
-              x="-1"
-            />
-            <g
-              display="none"
-              overflow="visible"
-              y="0"
-              x="0"
-              height="100%"
-              width="100%"
-              id="canvasGrid"
-            >
-              <rect
-                fill="url(#gridpattern)"
-                strokeWidth="0"
-                y="0"
-                x="0"
-                height="100%"
-                width="100%"
-              />
-            </g>
-          </g>
-          <g>
-            <title>Layer 1</title>
-          </g>
-        </svg>
-      ) : // <svg width="100" height="100" viewBox="0 0 100 100">
-      //   <rect width="100" height="100" rx="10" ry="10" fill="#CCC" />
-      // </svg>
-      null}
+        <div className="bg-gray-300 w-full h-full flex items-center justify-center">
+          <LoaderComponent type="Oval" timeout={0} height={60} width={60} />
+        </div>
+      ) : null}
       <img
-        style={!isImageLoaded ? { visibility: "hidden" } : {}}
+        style={!isImageLoaded ? hiddenStyle : {}}
         src={src}
         alt={alt}
         onLoad={() => setIsImageLoaded(true)}
       />
-    </>
+    </div>
   );
 };
 
